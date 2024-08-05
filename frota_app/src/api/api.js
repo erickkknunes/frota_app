a60ea8f21f7a07497d2a101e17193509dfe81942
Bruno Diedrich <brunodiedrich97@gmail.com>
2024-08-05 13:41:53 -0300

    refac: remove expo

--- frota_app/src/api/api.js
+++
@@ -1,9 +0,0 @@
-// api/api.js
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'http://localhost:3000', // URL base do seu servidor backend
-    timeout: 5000, // Tempo máximo de espera por uma resposta do servidor
-});
-
-export default instance;
